f5dbbd09cb623d5604d34e5d4ac8a5ccdd142db5
Callum <callum.taylor.reid@gmail.com>
2021-10-21 20:30:52 -0700

    For pr

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Crutons for Chat</Text>
+      <Text>Crutons for Chat App</Text>
       <StatusBar style="auto" />
     </View>
   );

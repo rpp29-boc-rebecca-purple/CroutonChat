1f65f395b213ee73930c30b8fe8e0cade4f91a94
Callum <callum.taylor.reid@gmail.com>
2021-10-21 20:29:56 -0700

    Made CrutonChat

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Crutons for</Text>
+      <Text>Crutons for Chat</Text>
       <StatusBar style="auto" />
     </View>
   );

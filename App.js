5705968a5462652cd9d17b963b1d6b6e3387779d
Matthew Saxe <mattsaxe17@gmail.com>
2021-11-01 22:36:09 -0400

    Add signup and login pages

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import React, {useState} from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import HomePage from './components/homePage.js'
-import SearchBar from './components/searchBar.js'
-import Friends from './components/friends.js'
-import ChatList from './components/chatlist.js'
-import CameraComponent from './components/camera.js'
-
+import HomePage from './components/homePage.js';
+import SearchBar from './components/searchBar.js';
+import Friends from './components/friends.js';
+import ChatList from './components/chatlist.js';
+import CameraComponent from './components/camera.js';
+import LoginPage from './components/loginPage';
+import SignupPage from './components/signupPage';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,9 +19,9 @@ export default function App() {
 
   // Functions that will nagivate to each componenet // acts like a router
   function HomeScreen() {
-      return (
-      <View style={{ flex: 1}}>
-        <HomePage name={name}/>
+    return (
+      <View style={{ flex: 1 }}>
+        <HomePage name={name} />
       </View>
     );
   }
@@ -28,75 +29,85 @@ export default function App() {
   function FriendsScreen() {
     return (
       <ScrollView>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "left" }}>
-        <SearchBar />
-        <Friends />
-      </View>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'left' }}>
+          <SearchBar />
+          <Friends />
+        </View>
       </ScrollView>
     );
   }
 
   function ChatScreen() {
     return (
-    <ScrollView>
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "left" }}>
-      <SearchBar />
-      <ChatList />
-    </View>
-    </ScrollView>
-  );
+      <ScrollView>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'left' }}>
+          <SearchBar />
+          <ChatList />
+        </View>
+      </ScrollView>
+    );
   }
 
   //  Camera function
   function CameraScreen() {
     return (
-      <View style={{ flex: 1}}>
-      <CameraComponent />
+      <View style={{ flex: 1 }}>
+        <CameraComponent />
       </View>
     );
   }
 
   function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text> Put Profile Component Here</Text>
-    </View>
-  );
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text> Put Profile Component Here</Text>
+      </View>
+    );
   }
-
 
   return (
     <NavigationContainer>
       <Tab.Navigator>
+      <Tab.Screen
+          name="Login"
+          component={LoginPage}
+          options={{
+            tabBarLabel: 'Login',
+            tabBarIcon: ({ color, size }) => <Ionicons name="login" color={color} size={size} />,
+          }}
+        />
+        <Tab.Screen
+          name="Signup"
+          component={SignupPage}
+          options={{
+            tabBarLabel: 'Sign Up',
+            tabBarIcon: ({ color, size }) => <Ionicons name="sign up" color={color} size={size} />,
+          }}
+        />
         <Tab.Screen
           name="Home"
           component={HomeScreen}
           options={{
             tabBarLabel: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" color={color} size={size} />
-            ),
-          }}/>
+            tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
+          }}
+        />
 
         <Tab.Screen
           name="Friends"
           component={FriendsScreen}
           options={{
             tabBarLabel: 'Friends',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="paw-outline" color={color} size={size} />
-            ),
+            tabBarIcon: ({ color, size }) => <Ionicons name="paw-outline" color={color} size={size} />,
           }}
-          />
+        />
 
         <Tab.Screen
           name="Camera"
           component={CameraScreen}
           options={{
             tabBarLabel: 'Camera',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="camera-outline" color={color} size={size} />
-            ),
+            tabBarIcon: ({ color, size }) => <Ionicons name="camera-outline" color={color} size={size} />,
           }}
         />
 
@@ -105,32 +116,29 @@ export default function App() {
           component={ChatScreen}
           options={{
             tabBarLabel: 'Chat',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="chatbubbles-outline" color={color} size={size} />
-            ),
-          }}/>
+            tabBarIcon: ({ color, size }) => <Ionicons name="chatbubbles-outline" color={color} size={size} />,
+          }}
+        />
 
         <Tab.Screen
           name="Profile"
           component={ProfileScreen}
           options={{
             tabBarLabel: 'Profile',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="person-circle-outline" color={color} size={size} />
-            ),
-          }}/>
+            tabBarIcon: ({ color, size }) => <Ionicons name="person-circle-outline" color={color} size={size} />,
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-
   cameraicon: {
-      width: 60,
-      height: 60,
-      borderRadius: 45,
-      borderWidth: 1,
-      resizeMode: 'contain'
-    },
+    width: 60,
+    height: 60,
+    borderRadius: 45,
+    borderWidth: 1,
+    resizeMode: 'contain',
+  },
 });
---
+++ components/signupPage.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View, TouchableOpacity, Text } from 'react-native';
+import OAuth from './oauth';
+
+function LoginPage(props) {
+  let [firstName, setFirstName] = useState('');
+  let [lastName, setLastName] = useState('');
+  let [email, setEmail] = useState('');
+  let [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.logo}>
+        <Text>LOGO</Text>
+      </View>
+      <TextInput style={styles.textInput} placeholder="First Name" onChangeText={text => setFirstName(text)} />
+      <TextInput style={styles.textInput} placeholder="Last Name" onChangeText={text => setLastName(text)} />
+      <TextInput style={styles.textInput} placeholder="Email" onChangeText={text => setEmail(text)} />
+      <TextInput style={styles.textInput} placeholder="Password" onChangeText={text => setEmail(text)} />
+
+      <TouchableOpacity
+        onPress={() => {
+          // go to sign up page
+        }}
+        style={styles.mainButton}
+      >
+        <Text style={styles.mainButtonText}>Sign up</Text>
+      </TouchableOpacity>
+
+      <View style={styles.horizontal}>
+        <Text style={styles.horizontalRule}>-------------------------------------</Text><Text>   OR   </Text><Text style={styles.horizontalRule}>-------------------------------------</Text>
+      </View>
+
+      <OAuth />
+
+      <TouchableOpacity
+        onPress={() => {
+          // go to sign up page
+        }}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Already have an account? Log in</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  horizontal: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+    maxHeight: 30
+  },
+  horizontalRule: {
+    letterSpacing: -2,
+    margin: 0,
+    padding: 0
+  },
+  mainButton: {
+    backgroundColor: 'grey',
+    padding: 10,
+    width: 275,
+    margin: 10,
+    borderRadius: 8
+  },
+  mainButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  button: {
+    margin: 10
+  },
+  buttonText: {
+    fontSize: 12
+  },
+  container: {
+    paddingHorizontal: 25,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    padding: 10,
+    margin: 8,
+  },
+  logo: {
+    width: 75,
+    maxHeight: 75,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'lightgrey',
+    margin: 50,
+  },
+});
+
+export default LoginPage;
